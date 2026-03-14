3c17268709631ab70adfe9aa2c797583eb6aa4e3
polectron <polectron@gmail.com>
2026-03-14 23:21:17 +0100

    implement async exchange rate data retrieval and refactor provider client handling, add business logic for smart exchange rate retrieval using different providers

--- currency/providers/__init__.py
+++ currency/providers/__init__.py
@@ -1,7 +1,8 @@
 import abc
 import datetime
 
-from mycurrency.core.models import Currency
+from currency.models import Currency, CurrencyProvider
+from currency.providers.currency_beacon import CurrencyBeaconCurrencyClient
 
 
 class CurrencyClient(abc.ABC):
@@ -14,13 +15,52 @@ class CurrencyClient(abc.ABC):
     ) -> float:
         raise NotImplementedError
 
+    @abc.abstractmethod
+    @staticmethod
+    async def get_exchange_rate_data_async(
+        source_currency: Currency,
+        exchanged_currency: Currency,
+        valuation_date: datetime.date,
+    ) -> float:
+        raise NotImplementedError
+
+
+def _get_provider_client(name: str) -> CurrencyClient:
+    """Load provider client class by name"""
+    provider_class = CLIENTS.get(name)
+    if not provider_class:
+        raise ValueError(f"Provider client {name} not found in registry")
+    return provider_class()
+
 
 def get_exchange_rate_data(
     source_currency: Currency,
     exchanged_currency: Currency,
     valuation_date: datetime.date,
-    provider: CurrencyClient,
+    provider: CurrencyProvider,
 ) -> float:
-    return provider.get_exchange_rate_data(source_currency, exchanged_currency, valuation_date)
+    client = _get_provider_client(provider.name)
+    return client.get_exchange_rate_data(
+        source_currency, exchanged_currency, valuation_date
+    )
 
-    
+
+CLIENTS = {"CurrencyBeacon": CurrencyBeaconCurrencyClient}
+
+
+def get_exchange_rate_data_smart(
+    source_currency: Currency,
+    exchanged_currency: Currency,
+    valuation_date: datetime.date,
+) -> float:
+    providers = CurrencyProvider.objects.filter(active=True).order_by("-priority").get()
+    exchange_rate: float | None = None
+    for provider in providers:
+        try:
+            exchange_rate = get_exchange_rate_data(
+                source_currency, exchanged_currency, valuation_date, provider
+            )
+            return exchange_rate
+        except Exception:
+            continue
+    raise ValueError("Couldn't fetch an exchange rate")
--- currency/providers/mock.py
+++ currency/providers/mock.py
@@ -1,8 +1,8 @@
 import datetime
 import random
 
-from mycurrency.core.currency_clients import CurrencyClient
-from mycurrency.core.models import Currency
+from currency.providers import CurrencyClient
+from currency.models import Currency
 
 
 class MockCurrencyClient(CurrencyClient):
@@ -13,3 +13,13 @@ class MockCurrencyClient(CurrencyClient):
         valuation_date: datetime.date,
     ) -> float:
         return random.random()
+    
+    @staticmethod
+    async def get_exchange_rate_data_async(
+        source_currency: Currency,
+        exchanged_currency: Currency,
+        valuation_date: datetime.date,
+    ) -> float:
+        # makes no sense to have an async method
+        # for just returning the result of a sync function call
+        raise NotImplementedError
