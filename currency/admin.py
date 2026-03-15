060693223e94282a8bca53a7c741c6b76769f3dc
polectron <polectron@gmail.com>
2026-03-15 19:52:36 +0100

    register CurrencyProvider in Django admin

--- currency/admin.py
+++ currency/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from currency.models import Currency, CurrencyExchangeRate
+from currency.models import Currency, CurrencyExchangeRate, CurrencyProvider
 
 admin.site.register(Currency)
 admin.site.register(CurrencyExchangeRate)
+admin.site.register(CurrencyProvider)
