6660272413681e355612c4385a6ae1ae457218c5
polectron <polectron@gmail.com>
2026-03-15 20:33:57 +0100

    - include 'id' field in Currency and CurrencyExchangeRate serializers - set source_currency and exchanged_currency as SlugRelatedField in CurrencyExchangeRateSerializer for showing currency code instead of url

--- currency/serializers.py
+++ currency/serializers.py
@@ -5,10 +5,12 @@ from rest_framework import serializers
 class CurrencySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Currency
-        fields = ["code", "name", "symbol"]
+        fields = ["id", "code", "name", "symbol"]
 
 
 class CurrencyExchangeRateSerializer(serializers.HyperlinkedModelSerializer):
+    source_currency = serializers.SlugRelatedField(slug_field='code', read_only=True)
+    exchanged_currency = serializers.SlugRelatedField(slug_field='code', read_only=True)
     class Meta:
         model = CurrencyExchangeRate
-        fields = ["source_currency", "exchanged_currency", "valuation_date", "rate_value"]
+        fields = ["id", "source_currency", "exchanged_currency", "valuation_date", "rate_value"]
